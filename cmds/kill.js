839f5e80e7d5e54dc80c08cdde2b5da0ad6a77c9
Gitindexjs <happypor10@gmail.com>
2020-06-24 14:17:03 +0100

    added a random color to kill cmd embed and fixed apostrophe issue with roundmimi

--- cmds/kill.js
+++ cmds/kill.js
@@ -3,7 +3,7 @@
 module.exports.run = (discord, client, msg, args) => {
     try{
         if(!args[1]){
-            return msg.reply('You did not provide a user')
+            return msg.reply('You didn\'t provide a user')
         } else {
             let killMember = msg.mentions.members.first() ||msg.guild.members.cache.find(member => member.user.tag === args.slice(1).join(' ').toLowerCase()) ||msg.guild.members.cache.find(member => member.nickname === args.slice(1).join(' ').toLowerCase()) || msg.guild.members.cache.find(member => member.user.username === args.slice(1).join(' ').toLowerCase())
             let killEmbed = new discord.MessageEmbed()
@@ -12,6 +12,7 @@ module.exports.run = (discord, client, msg, args) => {
                 .setTitle("Kill")
                 .setThumbnail(killMember.user.avatarURL({format: "png"}))
                 .setURL('https://www.liablecode.com')
+                .setColor("RANDOM")
                 .setFooter(`${client.user.username} ©`, client.user.avatarURL({format: "png"}))
             msg.channel.send(killEmbed);
         }    
