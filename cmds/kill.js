acb07e37afe27ff5e5304b320b73ca0a1161c184
Gitindexjs <happypor10@gmail.com>
2020-06-24 14:15:02 +0100

    created an embed response to kill cmd

--- cmds/kill.js
+++ cmds/kill.js
@@ -1,15 +1,21 @@
+// exports 
 
 module.exports.run = (discord, client, msg, args) => {
     try{
         if(!args[1]){
             return msg.reply('You did not provide a user')
-        }
-    else{
-        let killMember = msg.mentions.members.first() ||msg.guild.members.cache.find(member => member.user.tag === args.slice(1).join(' ').toLowerCase()) ||msg.guild.members.cache.find(member => member.nickname === args.slice(1).join(' ').toLowerCase()) || msg.guild.members.cache.find(member => member.user.username === args.slice(1).join(' ').toLowerCase())
-        let killEmbed = new discord.MessageEmbed()
-            .addField()
-    }    
-    } catch(err){
+        } else {
+            let killMember = msg.mentions.members.first() ||msg.guild.members.cache.find(member => member.user.tag === args.slice(1).join(' ').toLowerCase()) ||msg.guild.members.cache.find(member => member.nickname === args.slice(1).join(' ').toLowerCase()) || msg.guild.members.cache.find(member => member.user.username === args.slice(1).join(' ').toLowerCase())
+            let killEmbed = new discord.MessageEmbed()
+                .addField("killer", msg.member)
+                .addField("victim", killMember)
+                .setTitle("Kill")
+                .setThumbnail(killMember.user.avatarURL({format: "png"}))
+                .setURL('https://www.liablecode.com')
+                .setFooter(`${client.user.username} ©`, client.user.avatarURL({format: "png"}))
+            msg.channel.send(killEmbed);
+        }    
+    } catch(err) {
       console.log(err);
       msg.reply("internal error please report!");
     }
